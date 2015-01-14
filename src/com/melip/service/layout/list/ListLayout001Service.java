879a77eb734b2d53366ebe6e519ca047ce895ff7
naoya-suzuki <suzuki0617@gmail.com>
2015-01-14 17:08:44 +0900

    [add]ListLayout001用のサービスを新規作成

---
+++ src/com/melip/service/layout/list/ListLayout001Service.java
@@ -0,0 +1,24 @@
+package com.melip.service.layout.list;
+
+import com.melip.dto.screen.ScreenDto;
+import com.melip.service.common.AbstractService;
+
+/**
+ * ListLayout001のサービスです。
+ */
+public class ListLayout001Service extends AbstractService implements IListLayout001Service {
+
+  /**
+   * @see com.melip.service.layout.list.IListLayout001Service#getList(java.lang.Integer,
+   *      java.lang.Integer, java.lang.String)
+   */
+  @Override
+  public ScreenDto getList(Integer screenId, Integer targetId, String langDiv) {
+
+
+
+    return null;
+  }
+
+
+}
