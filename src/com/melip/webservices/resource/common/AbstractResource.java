e712261bd0fd1daa8dd56789af2e48e56ab2b98f
naoya-suzuki <suzuki0617@gmail.com>
2015-01-23 18:37:52 +0900

    [modify]abstractキーワードを付与

--- src/com/melip/webservices/resource/common/AbstractResource.java
+++ src/com/melip/webservices/resource/common/AbstractResource.java
@@ -4,8 +4,9 @@ package com.melip.webservices.resource.common;
  * リソースの基底クラスです。<br>
  * すべてのリソースはこのクラスを継承して作成します。
  */
-public class AbstractResource implements IResource {
+public abstract class AbstractResource implements IResource {
 
   /** JSONのメディアタイプ（UTF-8） */
   protected static final String MEDIA_TYPE_JSON = "application/json;charset=UTF-8";
+
 }
