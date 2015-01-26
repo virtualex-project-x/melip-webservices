de6c6c9de3d016252ccde4ae8c2463e889cd778d
naoya-suzuki <suzuki0617@gmail.com>
2015-01-26 19:42:24 +0900

    [add]地域サービスを新規作成

---
+++ src/com/melip/webservices/service/region/IRegionService.java
@@ -0,0 +1,52 @@
+package com.melip.webservices.service.region;
+
+import java.util.List;
+
+import com.melip.webservices.dto.RegionDto;
+
+/**
+ * 地域のサービスインタフェースです。
+ */
+public interface IRegionService {
+
+  /** DIコンテナ登録用の名称 */
+  public static final String SERVICE_NAME = "regionService";
+
+  /**
+   * 全地域情報を取得します。
+   * 
+   * @param langDiv 言語区分
+   * @return 地域DTOリスト
+   */
+  public List<RegionDto> getRegionList(String langDiv);
+
+  /**
+   * 全地域情報を取得します。<br>
+   * 属性情報は受け取った地域属性グループの情報のみです。
+   * 
+   * @param langDiv 言語区分
+   * @param regionAttrGrpId 地域属性グループID
+   * @return 地域DTOリスト
+   */
+  public List<RegionDto> getRegionList(String langDiv, Integer regionAttrGrpId);
+
+  /**
+   * 地域情報を取得します。
+   * 
+   * @param langDiv 言語区分
+   * @param regionId 地域ID
+   * @return 地域DTO
+   */
+  public RegionDto getRegion(String langDiv, Integer regionId);
+
+  /**
+   * 地域情報を取得します。<br>
+   * 属性情報は受け取った地域属性グループの情報のみです。
+   * 
+   * @param langDiv 言語区分
+   * @param regionId 地域ID
+   * @param regionAttrGrpId 地域属性グループID
+   * @return 地域DTOリスト
+   */
+  public RegionDto getRegion(String langDiv, Integer regionId, Integer regionAttrGrpId);
+}
---
+++ src/com/melip/webservices/service/region/RegionService.java
@@ -0,0 +1,80 @@
+package com.melip.webservices.service.region;
+
+import java.util.List;
+
+import com.melip.webservices.dto.RegionDto;
+import com.melip.webservices.service.common.AbstractService;
+
+/**
+ * 地域のサービスクラスです。
+ */
+public class RegionService extends AbstractService implements IRegionService {
+
+  /** 地域情報取得SQL_ID */
+  private String selectRegionSqlId;
+
+  /**
+   * @see com.melip.webservices.service.region.IRegionService#getRegionList(java.lang.String)
+   */
+  @Override
+  public List<RegionDto> getRegionList(String langDiv) {
+    return getRegionList(langDiv, null);
+  }
+
+  /**
+   * @see com.melip.webservices.service.region.IRegionService#getRegionList(java.lang.String,
+   *      java.lang.Integer)
+   */
+  @Override
+  public List<RegionDto> getRegionList(String langDiv, Integer regionAttrGrpId) {
+
+    RegionSearchCondition condition = new RegionSearchCondition();
+    condition.setLangDiv(langDiv);
+    condition.setRegionAttrGrpId(regionAttrGrpId);
+    List<RegionDto> regionDtoList = getSession().selectList(getSelectRegionSqlId(), condition);
+    return regionDtoList;
+  }
+
+  /**
+   * @see com.melip.webservices.service.region.IRegionService#getRegion(java.lang.String,
+   *      java.lang.Integer)
+   */
+  @Override
+  public RegionDto getRegion(String langDiv, Integer regionId) {
+    return getRegion(langDiv, regionId, null);
+  }
+
+  /**
+   * @see com.melip.webservices.service.region.IRegionService#getRegion(java.lang.String,
+   *      java.lang.Integer, java.lang.Integer)
+   */
+  @Override
+  public RegionDto getRegion(String langDiv, Integer regionId, Integer regionAttrGrpId) {
+
+    RegionSearchCondition condition = new RegionSearchCondition();
+    condition.setLangDiv(langDiv);
+    condition.setRegionId(regionId);
+    condition.setRegionAttrGrpId(regionAttrGrpId);
+    RegionDto regionDto = getSession().selectOne(getSelectRegionSqlId(), condition);
+    return regionDto;
+  }
+
+  /**
+   * 地域情報取得SQL_IDを取得します。
+   * 
+   * @return 地域情報取得SQL_ID
+   */
+  public String getSelectRegionSqlId() {
+    return selectRegionSqlId;
+  }
+
+  /**
+   * 地域情報取得SQL_IDを設定します。
+   * 
+   * @param selectRegionSqlId 地域情報取得SQL_ID
+   */
+  public void setSelectRegionSqlId(String selectRegionSqlId) {
+    this.selectRegionSqlId = selectRegionSqlId;
+  }
+
+}
