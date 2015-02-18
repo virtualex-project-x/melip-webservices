6d9bbf81cef8f3a1b61c600a64f6882fdff684f4
naoya-suzuki <suzuki0617@gmail.com>
2015-02-18 11:10:18 +0900

    [modify]構成を大幅に変更

--- src/com/melip/webservices/resource/FacilityResource.java
+++ src/com/melip/webservices/resource/FacilityResource.java
@@ -2,10 +2,7 @@ package com.melip.webservices.resource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -44,21 +41,6 @@ public class FacilityResource extends AbstractResource {
   /** 昇順を表す文字列 */
   private static final String STR_ASC = "asc";
 
-  /** 言語区分 */
-  private String langDiv;
-  /** 属性グループリスト */
-  private List<String> attrGrpList;
-  /** 地域ID */
-  private Integer regionId;
-  /** 検索条件マップ */
-  private Map<String, String> conditionMap;
-  /** 開始位置 */
-  private Integer index;
-  /** 件数 */
-  private Integer count;
-  /** ソートキーマップ */
-  private Map<String, Boolean> orderMap;
-
   /**
    * 施設情報をJSON形式で取得します。
    * 
@@ -82,32 +64,34 @@ public class FacilityResource extends AbstractResource {
     // HTTPエラーコードを返す方法
     // エラー用JSONオブジェクトを定義してメッセージを登録する
 
+    // パラメータチェック
     checkParameters(langDiv, attrGrp, regionId, condition, index, count, order);
 
-    FacilitySearchConditionDto facilityCondition = createFacilitySearchCondition();
-    QueryCondition queryCondition = new QueryCondition(getLangDiv(), facilityCondition);
-    if (null != getIndex()) {
-      queryCondition.setIndex(getIndex());
-    }
-    if (null != getCount()) {
-      queryCondition.setCount(getCount());
-    }
+    // 検索条件生成
+    FacilitySearchConditionDto facilityCondition =
+        createFacilitySearchCondition(attrGrp, regionId, condition);
+    QueryCondition queryCondition = createQueryCondition(langDiv, index, count, order);
+    queryCondition.setParam(facilityCondition);
 
+    // 施設DTOリスト取得
     IFacilityService service =
         BeanCreator.getBean(IFacilityService.SERVICE_NAME, IFacilityService.class);
     DtoList<FacilityDto> list = service.getFacilityDtoList(queryCondition);
 
-    ResourceDto dto = new ResourceDto();
-    dto.setAllCount(list.getAllCount());
-    dto.setIndex(list.getIndex());
-    dto.setCount(list.getCount());
-    dto.setDtoList(list.getDtoList());
-    dto.setEntity(ENTITY_NAME);
-
-    return dto;
+    return createResourceDto(list);
   }
 
-  // TODO:Javadoc
+  /**
+   * パラメータチェックを行います。
+   * 
+   * @param langDiv 言語区分
+   * @param attrGrp 属性グループ
+   * @param regionId 地域ID
+   * @param condition 検索条件
+   * @param index 開始位置
+   * @param count 件数
+   * @param order ソートキー
+   */
   private void checkParameters(String langDiv, String attrGrp, String regionId, String condition,
       String index, String count, String order) {
 
@@ -122,199 +106,105 @@ public class FacilityResource extends AbstractResource {
       log.debug("  ソートキー   -> " + order);
     }
 
-    // TODO:必須チェックなど
-
-    // 言語区分
-    setLangDiv(langDiv);
-    // 属性グループリスト
-    if (StringUtils.isNotEmpty(attrGrp)) {
-      setAttrGrpList(Arrays.asList(attrGrp.split(SEPARATOR_ITEM)));
-    }
-    // 地域ID
-    if (StringUtils.isNotEmpty(regionId)) {
-      setRegionId(Integer.valueOf(regionId));
-    }
-    // 検索条件マップ
-    if (StringUtils.isNotEmpty(condition)) {
-      Map<String, String> conditionMap = new HashMap<String, String>();
-      String[] conditions = condition.split(SEPARATOR_ITEM);
-      for (String conditionPair : conditions) {
-        String[] values = conditionPair.split(SEPARATOR_KEY_VALUE);
-        conditionMap.put(values[0], values[1]);
-      }
-      setConditionMap(conditionMap);
-    }
-    // 開始位置
-    if (StringUtils.isNotEmpty(index)) {
-      setIndex(Integer.valueOf(index));
-    }
-    // 件数
-    if (StringUtils.isNotEmpty(count)) {
-      setCount(Integer.valueOf(count));
-    }
-    // ソートキーマップ
-    if (StringUtils.isNotEmpty(order)) {
-      Map<String, Boolean> orderMap = new LinkedHashMap<String, Boolean>();
-      String[] orderKeys = order.split(SEPARATOR_ITEM);
-      for (String orderPair : orderKeys) {
-        String[] values = orderPair.split(SEPARATOR_KEY_VALUE);
-        Boolean isAsc = STR_ASC.equals(values[1]) ? true : false;
-        orderMap.put(values[0], isAsc);
-      }
-      setOrderMap(orderMap);
-    }
+    // TODO:必須チェック、数値チェックなど
   }
 
   /**
    * 施設検索条件DTOを生成します。
    * 
-   * @return 施設検索条件DTO
+   * @param attrGrp 属性グループ
+   * @param regionId 地域ID
+   * @param condition 検索条件
+   * @return
    */
-  private FacilitySearchConditionDto createFacilitySearchCondition() {
+  private FacilitySearchConditionDto createFacilitySearchCondition(String attrGrp, String regionId,
+      String condition) {
 
-    FacilitySearchConditionDto dto = new FacilitySearchConditionDto();
-    dto.setTargetAttrGrpAliasList(getAttrGrpList());
-    dto.setRegionId(getRegionId());
-    if (null != getConditionMap()) {
-      List<AttrGrpSearchConditionDto> attrGrpConditionList =
-          new ArrayList<AttrGrpSearchConditionDto>();
-      for (Map.Entry<String, String> entiry : getConditionMap().entrySet()) {
-        AttrGrpSearchConditionDto attrGrpCondition = new AttrGrpSearchConditionDto();
-        attrGrpCondition.setAttrGrpAlias(entiry.getKey());
-        attrGrpCondition.setVal(entiry.getValue());
-        attrGrpConditionList.add(attrGrpCondition);
-      }
-      dto.setAttrGrpSearchConditionDtoList(attrGrpConditionList);
-      dto.setNumOfSearchCondition(attrGrpConditionList.size());
+    FacilitySearchConditionDto facilityConditionDto = new FacilitySearchConditionDto();
+
+    // 取得対象属性グループエイリアスリスト
+    if (StringUtils.isNotEmpty(attrGrp)) {
+      facilityConditionDto.setTargetAttrGrpAliasList(Arrays.asList(attrGrp.split(SEPARATOR_ITEM)));
     }
 
-    return dto;
+    // 地域ID
+    if (StringUtils.isNotEmpty(regionId)) {
+      facilityConditionDto.setRegionId(Integer.valueOf(regionId));
+    }
+
+    // 属性グループ検索条件DTOリスト
+    // 属性グループ検索条件の個数
+    if (StringUtils.isNotEmpty(condition)) {
+      List<AttrGrpSearchConditionDto> attrGrpConditionList =
+          new ArrayList<AttrGrpSearchConditionDto>();
+      String[] conditions = condition.split(SEPARATOR_ITEM);
+      for (String conditionPair : conditions) {
+        String[] values = conditionPair.split(SEPARATOR_KEY_VALUE);
+        AttrGrpSearchConditionDto attrGrpCondition = new AttrGrpSearchConditionDto();
+        attrGrpCondition.setAttrGrpAlias(values[0]);
+        attrGrpCondition.setVal(values[1]);
+        attrGrpConditionList.add(attrGrpCondition);
+      }
+      facilityConditionDto.setAttrGrpSearchConditionDtoList(attrGrpConditionList);
+      facilityConditionDto.setNumOfSearchCondition(attrGrpConditionList.size());
+    }
+
+    return facilityConditionDto;
   }
 
   /**
-   * 言語区分を取得します。
-   * 
-   * @return 言語区分
-   */
-  public String getLangDiv() {
-    return langDiv;
-  }
-
-  /**
-   * 言語区分を設定します。
+   * 検索条件を生成します。
    * 
    * @param langDiv 言語区分
-   */
-  public void setLangDiv(String langDiv) {
-    this.langDiv = langDiv;
-  }
-
-  /**
-   * 属性グループリストを取得します。
-   * 
-   * @return 属性グループリスト
-   */
-  public List<String> getAttrGrpList() {
-    return attrGrpList;
-  }
-
-  /**
-   * 属性グループリストを設定します。
-   * 
-   * @param attrGrpList 属性グループリスト
-   */
-  public void setAttrGrpList(List<String> attrGrpList) {
-    this.attrGrpList = attrGrpList;
-  }
-
-  /**
-   * 地域IDを取得します。
-   * 
-   * @return 地域ID
-   */
-  public Integer getRegionId() {
-    return regionId;
-  }
-
-  /**
-   * 地域IDを設定します。
-   * 
-   * @param regionId 地域ID
-   */
-  public void setRegionId(Integer regionId) {
-    this.regionId = regionId;
-  }
-
-  /**
-   * 検索条件マップを取得します。
-   * 
-   * @return 検索条件マップ
-   */
-  public Map<String, String> getConditionMap() {
-    return conditionMap;
-  }
-
-  /**
-   * 検索条件マップを設定します。
-   * 
-   * @param conditionMap 検索条件マップ
-   */
-  public void setConditionMap(Map<String, String> conditionMap) {
-    this.conditionMap = conditionMap;
-  }
-
-  /**
-   * 開始位置を取得します。
-   * 
-   * @return 開始位置
-   */
-  public Integer getIndex() {
-    return index;
-  }
-
-  /**
-   * 開始位置を設定します。
-   * 
    * @param index 開始位置
-   */
-  public void setIndex(Integer index) {
-    this.index = index;
-  }
-
-  /**
-   * 件数を取得します。
-   * 
-   * @return 件数
-   */
-  public Integer getCount() {
-    return count;
-  }
-
-  /**
-   * 件数を設定します。
-   * 
    * @param count 件数
+   * @param order ソートキー
+   * @return 検索条件
    */
-  public void setCount(Integer count) {
-    this.count = count;
+  private QueryCondition createQueryCondition(String langDiv, String index, String count,
+      String order) {
+
+    QueryCondition queryCondition = new QueryCondition(langDiv);
+
+    // 開始位置
+    if (StringUtils.isNotEmpty(index)) {
+      queryCondition.setIndex(Integer.valueOf(index));
+    }
+
+    // 取得件数
+    if (StringUtils.isNotEmpty(count)) {
+      queryCondition.setCount(Integer.valueOf(count));
+    }
+
+    // ソートキー
+    if (StringUtils.isNotEmpty(order)) {
+      String[] orderKeys = order.split(SEPARATOR_ITEM);
+      for (String orderPair : orderKeys) {
+        String[] values = orderPair.split(SEPARATOR_KEY_VALUE);
+        Boolean isAsc = STR_ASC.equals(values[1]);
+        queryCondition.addOrderBy(values[0], isAsc);
+      }
+    }
+
+    return queryCondition;
   }
 
   /**
-   * ソートキーマップを取得します。
+   * リソースDTOを生成します。
    * 
-   * @return ソートキーマップ
+   * @param list DTOリスト
+   * @return リソースDTO
    */
-  public Map<String, Boolean> getOrderMap() {
-    return orderMap;
-  }
+  private ResourceDto createResourceDto(DtoList<FacilityDto> list) {
 
-  /**
-   * ソートキーマップを設定します。
-   * 
-   * @param orderMap ソートキーマップ
-   */
-  public void setOrderMap(Map<String, Boolean> orderMap) {
-    this.orderMap = orderMap;
+    ResourceDto resourceDto = new ResourceDto();
+    resourceDto.setAllCount(list.getAllCount());
+    resourceDto.setIndex(list.getIndex());
+    resourceDto.setCount(list.getCount());
+    resourceDto.setDtoList(list.getDtoList());
+    resourceDto.setEntity(ENTITY_NAME);
+
+    return resourceDto;
   }
 
 }
